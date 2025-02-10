48f4740a8ac30442b2621779e2c2f5b23c43e686
benjaminsampica <benjamin.sampica@gmail.com>
2025-02-10 10:57:52 -0600

    more debug

--- src/DungeonDates.Client/Pages/Add.razor.cs
+++ src/DungeonDates.Client/Pages/Add.razor.cs
@@ -5,11 +5,13 @@ namespace DungeonDates.Client.Pages;
 public partial class Add(HttpClient httpClient)
 {
     private string? _content;
+    private string? _uri;
     
     public async Task CallApiAsync()
     {
         var response = await httpClient.GetAsync("/add");
-        
+
+        _uri = response.RequestMessage?.RequestUri?.AbsoluteUri;
         _content = await response.Content.ReadAsStringAsync();
     }
 }
