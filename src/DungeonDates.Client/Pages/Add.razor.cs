90fbd4eae2b54d57b8daddb6471bed5cea31aed4
benjaminsampica <benjamin.sampica@gmail.com>
2025-02-10 15:49:41 -0600

    More styling

--- src/DungeonDates.Client/Pages/Add.razor.cs
+++ src/DungeonDates.Client/Pages/Add.razor.cs
@@ -1,17 +1,14 @@
 using DungeonDates.Shared;
 
-
 namespace DungeonDates.Client.Pages;
 
 public partial class Add(HttpClient httpClient)
 {
+    private readonly IList<AddCalendarDate> _calendarDates = [];
     private RadzenScheduler<AddCalendarDate> _scheduler = null!;
-
-    private IList<AddCalendarDate> _calendarDates = [];
     
     private void OnSlotRender(SchedulerSlotRenderEventArgs args)
     {
-        // Highlight today
         if (args.Start.Date == DateTime.Today)
         {
             args.Attributes["style"] = "background: var(--rz-scheduler-highlight-background-color, rgba(255,220,40,.2));";
@@ -20,7 +17,8 @@ public partial class Add(HttpClient httpClient)
     
     private async Task OnSlotSelectAsync(SchedulerSlotSelectEventArgs args)
     {
-        var newCalendarDate = new AddCalendarDate { Start = args.Start, End = args.Start.AddDays(1).AddSeconds(-1)};
+        var day = args.Start.Date;
+        var newCalendarDate = new AddCalendarDate { Start = day, End = day.AddDays(1).AddSeconds(-1)};
         
         _calendarDates.Add(newCalendarDate);
         await _scheduler.Reload();
@@ -30,10 +28,5 @@ public partial class Add(HttpClient httpClient)
     {
         _calendarDates.Remove(args.Data);
         await _scheduler.Reload();
-    }
-    
-    public async Task CallApiAsync()
-    {
-        var response = await httpClient.GetAsync("add");
     }
 }
